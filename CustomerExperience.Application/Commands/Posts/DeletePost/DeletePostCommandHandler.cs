6bd4b27a573ce3bf50d99a75c1255abf55eeaee4
maissukkarieh21 <maissukkarieh@hotmail.com>
2023-12-28 16:05:13 +0300

    DeletePost

---
+++ CustomerExperience.Application/Commands/Posts/DeletePost/DeletePostCommandHandler.cs
@@ -0,0 +1,24 @@
+﻿using CustomerExperience.Domain.PostAggregate;
+using CustomerExperience.Packages;
+using MediatR;
+
+namespace CustomerExperience.Application.Commands.Posts.DeletePost
+{
+    internal sealed class DeletePostCommandHandler : IRequestHandler<DeletePostCommand, Unit>
+    {
+        private readonly IPostRepository _postRepository;
+        private readonly IUnitOfWork _unitOfWork;
+        public DeletePostCommandHandler(IPostRepository postRepository, IUnitOfWork unitOfWork)
+        {
+            _postRepository = postRepository;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<Unit> Handle(DeletePostCommand command, CancellationToken cancellationToken)
+        {
+            var postToDelete = await _postRepository.GetAsync(command.Id);
+            postToDelete.DeletePost(command.Id);
+            await _postRepository.DeleteAsync(postToDelete);
+            return Unit.Value;
+        }
+    }
+}
