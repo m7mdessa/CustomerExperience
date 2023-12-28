379748ad1927c01f9d6a92a5d9c67ec8a71f2c5e
Mohammad Issa <m.Moien.ext@Tahaluf.ae>
2023-12-28 15:59:58 +0300

    nn

--- CustomerExperience.Domain/RoleAggregate/Role.cs
+++ CustomerExperience.Domain/RoleAggregate/Role.cs
@@ -1,10 +1,4 @@
-﻿using CustomerExperience.Domain.PostAggregate;
-using CustomerExperience.Packages;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using CustomerExperience.Packages;
 
 namespace CustomerExperience.Domain.RoleAggregate
 {
