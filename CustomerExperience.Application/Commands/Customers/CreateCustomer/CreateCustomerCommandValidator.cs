d0945a870c0feb6cae12251b7fcb691e9d299681
Mohammad Issa <m.Moien.ext@Tahaluf.ae>
2023-12-29 07:32:04 +0300

    ....

---
+++ CustomerExperience.Application/Commands/Customers/CreateCustomer/CreateCustomerCommandValidator.cs
@@ -0,0 +1,22 @@
+﻿using FluentValidation;
+
+namespace CustomerExperience.Application.Commands.Customers.CreateCustomer
+{
+  
+
+    public class CreateCustomerCommandValidator : AbstractValidator<CreateCustomerCommand>
+    {
+        public CreateCustomerCommandValidator()
+        {
+            RuleFor(x => x.FirstName)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(50);
+
+            RuleFor(x => x.LastName)
+                .NotNull()
+                .NotEmpty()
+                .MaximumLength(400);
+        }
+    }
+}
--- CustomerExperience.Start/Program.cs
+++ CustomerExperience.Start/Program.cs
@@ -6,8 +6,11 @@ using CustomerExperience.Domain.Shared;
 using CustomerExperience.Infra;
 using CustomerExperience.Infra.Repositories;
 using Mapster;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection;
+using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -36,6 +39,36 @@ builder.Services.AddSingleton(config);
 
 
 builder.Services.AddMediatR(cfg => cfg.RegisterServicesFromAssembly(typeof(AssemblyReference).Assembly));
+
+
+builder.Services.AddAuthentication(opt => {
+    opt.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+    opt.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+})
+   .AddJwtBearer(options =>
+   {
+       options.TokenValidationParameters = new TokenValidationParameters
+       {
+           ValidateIssuer = true,
+           ValidateAudience = true,
+           ValidateLifetime = true,
+           ValidateIssuerSigningKey = true,
+           IssuerSigningKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes("superSecretKey@345"))
+       };
+   });
+
+
+builder.Services.AddCors(corsOptions =>
+{
+    corsOptions.AddPolicy("policy",
+    builder =>
+    {
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader();
+    });
+});
+
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -46,6 +79,10 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+app.UseCors("policy");
+
+app.UseAuthentication();
 
 app.UseAuthorization();
 
