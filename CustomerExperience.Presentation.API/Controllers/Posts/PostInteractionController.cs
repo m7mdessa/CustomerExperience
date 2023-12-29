07511abad148db617aa3872ff8a2a263cd77fec3
Mohammad Issa <m.Moien.ext@Tahaluf.ae>
2023-12-29 08:34:23 +0300

    ......

---
+++ CustomerExperience.Presentation.API/Controllers/Posts/PostInteractionController.cs
@@ -0,0 +1,39 @@
+﻿using CustomerExperience.Application.Commands.Posts.PostInteraction.React;
+using CustomerExperience.Application.Commands.Posts.PostInteraction.UpdateReact;
+using Microsoft.AspNetCore.Mvc;
+using MediatR;
+
+namespace CustomerExperience.Presentation.API.Controllers.Posts
+{
+   
+
+    public partial class PostsController
+    {
+        #region Commands
+
+
+        [HttpPost("{postId}/postInteractions")]
+        public async Task<ActionResult> React(int postId,
+            [FromBody] ReactCommand command)
+        {
+            command.PostId = postId;
+
+            return Ok(await _mediator.Send(command));
+        }
+
+        [HttpPut("{postId}/postInteractions/{id}")]
+        public async Task<ActionResult> UpdateReact(int postId, int id,
+        [FromBody] UpdateReactCommand command)
+        {
+            command.PostId = postId;
+            command.Id = id;
+
+            return Ok(await _mediator.Send(command));
+        }
+
+       
+        #endregion
+
+
+    }
+}
---
+++ CustomerExperience.Presentation.API/Controllers/Posts/PostsController.cs
@@ -0,0 +1,47 @@
+﻿using MediatR;
+using Microsoft.AspNetCore.Mvc;
+using CustomerExperience.Application.Commands.Posts.UpdatePost;
+using CustomerExperience.Application.Commands.Posts.CreatePost;
+
+
+namespace PostExperience.Presentation.API.Controllers.Posts
+{
+   
+
+    public partial class PostsController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+
+        public PostsController(IMediator mediator)
+        {
+            _mediator = mediator;
+
+        }
+
+        #region Commands
+
+        [HttpPost("CreatePost")]
+        public async Task<ActionResult> CreatePost([FromBody] CreatePostCommand command)
+        {
+
+            return Ok(await _mediator.Send(command));
+
+        }
+
+
+        [HttpPut("{id}")]
+        public async Task<ActionResult> UpdatePost(int id,
+            [FromBody] UpdatePostCommand command)
+        {
+            command.Id = id;
+            return Ok(await _mediator.Send(command));
+        }
+
+
+        #endregion
+
+
+
+    }
+}
