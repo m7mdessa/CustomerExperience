0dba0b1114a2814ae58c061c1b40c58c482fb5eb
maissukkarieh21 <maissukkarieh@hotmail.com>
2023-12-29 10:22:13 +0300

    GetAllPosts

---
+++ CustomerExperience.Application/Queries/Posts/GetAllPosts/getAllPostsDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomerExperience.Application.Queries.Posts.GetAllPosts
+{
+    internal class getAllPostsDto
+    {
+    }
+}
